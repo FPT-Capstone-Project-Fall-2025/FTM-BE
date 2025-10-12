4ab5797499c700b42129fc095803b9f6f739762d
Tran Phi Long <longtpit25732gmail.com>
2025-10-12 15:00:04 +0700

    rebase from develop

---
+++ FTM.Domain/DTOss/FamilyTree/FamilyTreeDataTableDto.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace FTM.Domain.DTOs.FamilyTree
+{
+    public class FamilyTreeDataTableDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        public bool IsActive { get; set; }
+        public int? GPModeCode { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? LastModifiedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public string LastModifiedBy { get; set; }
+        public int MemberCount { get; set; }
+    }
+}
