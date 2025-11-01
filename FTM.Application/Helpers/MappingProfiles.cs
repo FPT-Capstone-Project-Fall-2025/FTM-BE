3be26dc59fb2bd616f0743783148ccbd55ff4dd8
Nguyen Quang Hau <quanghau6622@gmail.com>
2025-11-01 22:24:53 +0700

    Update File Path For Avatar

--- FTM.Application/Helpers/MappingProfiles.cs
+++ FTM.Application/Helpers/MappingProfiles.cs
@@ -37,7 +37,7 @@ namespace FTM.Application.Helpers
                                     Value = gr.OrderBy(x => x.ToFTMember.Birthday).Select(rFrom => rFrom.ToFTMember.Id).ToArray()
                                 })))
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Fullname))
-                .ForMember(dest => dest.Avatar, opt => opt.MapFrom(src => src.FTMemberFiles.FirstOrDefault(f => f.Title.Contains("Avatar"))))
+                .ForMember(dest => dest.Avatar, opt => opt.MapFrom(src => src.FTMemberFiles.First(f => f.Title.Contains("Avatar")).FilePath))
                 .AfterMap((src, desc) => {
                     if (desc.Partners.IsNullOrEmpty()) {
                         desc.Partners = src.FTRelationshipTo.Where(x => x.CategoryCode == FTRelationshipCategory.PARTNER && x.ToFTMemberId == src.Id).Select(x => x.FromFTMember.Id).ToList();
